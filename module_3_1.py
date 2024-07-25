43a1c7e96fd3dc3c1b123b77bd9bac0b5b995315
RustamBurambaev <www120991@mail.ru>
2024-07-24 19:57:20 -0700

    исправил

--- module_3_1.py
+++ module_3_1.py
@@ -1,15 +1,23 @@
 calls = 0
+
+
 def count_calls():
     global calls
     calls += 1
+
+
 def string_info(string):
     count_calls()
-    return (len(string), string.upper(), string.lower())
-def is_contains (string, list_to_search):
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
     count_calls()
     for s in list_to_search:
-        if string in s.lower() or s.lower() in string.lower():
+        if string.lower() in s.lower() or s.lower() in string.lower():
             return True
+
+
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
